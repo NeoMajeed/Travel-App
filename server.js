d5e021ec7a30572c561a057b82a2693c18042a7a
Ubdo15 <ubdo15@egmail.com>
2022-11-09 17:51:08 +0300

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const env = require('dotenv');
+env.config();
+const fetch = require('node-fetch');
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 const port = 8080;
+
+//baseApis
+const pixabayApi = 'https://pixabay.com/api/?key=';
+const weatherbitApi = 'https://api.weatherbit.io/v2.0/forecast/daily?';
+const geonamesApi = 'http://api.geonames.org/searchJSON?q=';
 
 // Require Express to run server and routes
 const express = require("express");
@@ -18,6 +26,7 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require("cors");
+const { urlencoded } = require('body-parser');
 app.use(cors());
 
 // Initialize the main project folder
@@ -29,19 +38,97 @@ app.listen(port, ()=>{
     console.log("server working on port: " + port);
 })
 
-// Get route
-app.get("/all", (req,res)=>{
-    res.send(projectData);
-})
+// // Get route
+// app.get("/all", (req,res)=>{
+//     res.send(projectData);
+// })
 
 
 // Post route
-app.post("/add", (req,res)=>{
-    projectData.city = req.body.city;
-    projectData.country = req.body.country;
-    projectData.date = req.body.date;
-    projectData.temp = req.body.temp;
-    res.send(projectData);
+app.post("/add", async(req,res)=>{
+    const city = req.body.city;
+    projectData.city = city;
+    const date = req.body.start_date;
+    const geodata = await geonames(city)
+    if (geodata) {
+        const lat = geodata.lat;
+        const lng = geodata.lng;
+        const country = geodata.countryName
+        await weatherbit(lat, lng, date);
+        await pixabay(city, country);
+        console.log(projectData);
+        res.send(projectData);
+    } else {
+        res.send("error");
+    }
 })
 
+//get route for geonames
+async function geonames(city) {
+    console.log(city);
+    
+    const request = geonamesApi + encodeURIComponent(city) + "&username=" + process.env.geoApiKey;
+    console.log(request);
+    const response = await fetch(request);
+    try {
+        //console.log(response[0]);
+        const data = await response.json();
+        //console.log(data[0]);
+        if (data.totalResultsCount > 0) {
+            projectData.country = data.geonames[0].countryName;
+            return data.geonames[0];
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.log("error", error);
+    }
+}
 
+//get route for weatherbit
+async function weatherbit(lat, lng, date) {
+    console.log(lat, lng);
+    start_date = "2022-11-11";
+    end_date = "2022-11-12";
+    const request = `${weatherbitApi}lat=${lat}&lon=${lng}&start_date=${start_date}&end_date=${end_date}&key=${process.env.weatherApiKey}` ;
+    console.log(request);
+    const response = await fetch(request);
+    try {
+        const data = await response.json();
+        projectData.min = data.data[0].min_temp;
+        projectData.max = data.data[0].max_temp;
+        projectData.description = data.data[0].weather.description;
+        console.log(data.data[0].min_temp);
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+
+
+//get route for pixabay
+async function pixabay(city, country) {
+    const request = pixabayApi + process.env.pixabayApiKey + '&q=' + encodeURIComponent(city) + '&orientation=horizontal&image_type=photo';
+    console.log(request);
+    const response = await fetch(request);
+    const data = await response.json();
+    if (data.totalHits > 0) {
+        let hits = data.hits
+        console.log(hits);
+        let filterdhits = hits.filter(hit => hit.tags.includes(city) || hit.tags.includes("city"));
+        console.log(filterdhits);
+        filterdhits.sort((a,b) => b.views - a.views);
+        projectData.image = filterdhits[0].webformatURL;
+    }else {
+        const request = pixabayApi + process.env.pixabayApiKey + '&q=' + encodeURIComponent(country) + '&orientation=horizontal&image_type=photo';
+        console.log(request);
+        const response = await fetch(request);
+        const data = await response.json();
+        if (data.totalHits > 0) {
+            let hits = data.hits
+            projectData.image = hits[0].webformatURL;
+        }else{
+            projectData.image = 'https://cdn.pixabay.com/photo/2018/02/27/06/30/skyscrapers-3184798_960_720.jpg';
+        }
+    }   
+}
--- website/app.js
+++ website/app.js
@@ -2,18 +2,43 @@
 /* Global Variables */
 const apiKey = "ca7ac655b2e15c8d34736322028a2e6f";
 const openWeather = "https://api.openweathermap.org/data/2.5/weather?zip=";
-const zip = document.querySelector("#zip");
-const feelings = document.querySelector("#feelings");
-const date = document.querySelector("#date");
-const temp = document.querySelector("#temp");
-const content = document.querySelector("#content");
+const temp = document.querySelector(".temp");
+const description = document.querySelector(".description");
 const city = document.querySelector("#city");
+const inputcity = document.querySelector(".Icity");
 const generate = document.querySelector("#generate");
+const img = document.querySelector("img");
+const figure = document.querySelector("figure");
+const app = document.querySelector(".app");
+const rmv = document.querySelector(".rmv");
+const date = document.querySelector("#date");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+ 1 +'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+ 1 +'/'+ d.getDate()+'/'+ d.getFullYear();
 
+function updateUI(data){
+    img.src = data.image; 
+    inputcity.innerHTML = data.city.toUpperCase() + ", " + data.country;
+    temp.innerHTML = "Low "  + data.min + "°C  -  High " + data.max + "°C";
+    description.innerHTML = data.description;
+    app.classList.add("flip")
+}
+
+window.addEventListener("load", function(){
+    console.log("window loaded");
+    if(this.localStorage.getItem("data") != null){
+        console.log("local storage is not empty");
+        data = JSON.parse(localStorage.getItem("data"));
+        updateUI(data);
+    }else{
+        console.log("local storage is empty");
+    }
+});
+
+function loading(){
+
+}
 
 //fetch data
 const postData = async (url = "", data = {})=>{
@@ -28,56 +53,59 @@ const postData = async (url = "", data = {})=>{
       body: JSON.stringify(data)   
     })
     try {
-      const newData = await response.json();
-      return newData;
+      const Data = await response.json();
+      return Data;
     } catch (error) {
       console.log("error", error);
     }
 }
 
 // get data from the server
-const retrieveData = async () =>{
- const request = await fetch('/all');
- try {
- // Transform into JSON
- const allData = await request.json()
- console.log(allData)
- // Write updated data to DOM elements
- city.innerHTML = 'City: ' + allData.city + ", " + allData.country;
- temp.innerHTML = 'Temperature: ' + Math.round(allData.temp) + ' °C';
- //content.innerHTML = 'I Feel: ' + allData.feeling;
- date.innerHTML = 'Date: ' + allData.date;
- }
- catch(error) {
-   console.log("error", error);
- }
-}
-
+// const retrieveData = async () =>{
+//   const request = await fetch('/all');
+//   return request.json();
+// }
 // get data from Api
-const getApiData=async(url)=>{
-    const response = await fetch(url)
-    try {   
-     const data =await response.json();  
-     console.log(data)
-     return data;
-    }catch (error){  
-     console.log("error",error);
-    }   
-}
+// const getApiData=async(url)=>{
+//     const response = await fetch(url)
+//     try {   
+//      const data =await response.json();  
+//      console.log(data)
+//      return data;
+//     }catch (error){  
+//      console.log("error",error);
+//     }   
+// }
 
 
-// Click Event 
-generate.addEventListener("click", ()=>{
-  if(isNaN(zip.value) || zip.value == ''){
-    alert("Please Enter a zip code")
-  } else{
-    const URL = `${openWeather}${zip.value}&appid=${apiKey}&units=metric`;
+// Click Events 
+
+//Post Event
+generate.addEventListener("click", async()=>{
+  if(city.value == ''){
+    alert("Please Enter a city name")
+  } else if (date.value == ''){
+    alert("Please Enter a date")
+  } else {
+    console.log(date.value);
     // send data to the server
-    getApiData(URL)
-    .then(data => postData("/add", {city:data.name, country:data.sys.country, date:newDate, temp:data.main.temp})
-    // get data from the server and update the DOM 
-    .then(retrieveData())
-    );
+    const object = await postData("/add", {city:city.value, date: date.value});
+    if(object){
+      localStorage.setItem("data", JSON.stringify(object));
+      updateUI(object);
+      // get data from the server and update the DOM 
+      console.log(object)      
+    }else{
+      alert("something went wrong, please try again with another city name")
+    }
+
   }
 })
+
+// Back Event
+rmv.addEventListener("click", ()=>{
+  localStorage.removeItem("data");
+  app.classList.remove("flip")
+})
+
 
